069743691addaefc81053030272876cc5fadba30
wilkerpaz <wilkerpaz@hotmail.com>
2019-12-03 21:30:38 -0300

    Ajuste timestamp para hora, ajuste .env, ajuste forward

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Bot lab do grupo DevOpsPBS.
 # Features:
 - [x] getid - Get ID do usuário;
 - [x] hello - Manda uma mensagem para o usuário;
-- [ ] browser - Abre o browser na maquina;
+- [x] browser - Abre o browser na maquina;
 - [x] now - Exiber data e hora;
 - [ ] eventos - Exibe os eventos de uma lista;
-- [ ] forward - Redirecionar mensagens do grupo para usuário especifico;
+- [x] forward - Redirecionar mensagens do grupo para usuário especifico;
 - [x] site - Retorna a url de um Site;
 - [x] links - Retorna uma lista de url's;
 - [X] previsao_do_tempo - Retorna a precisão do tempo de um cep;
--- lab_previsao_do_tempo.py
+++ lab_previsao_do_tempo.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from telegram.ext import Updater, CommandHandler
 from decouple import config
 from collections import namedtuple
@@ -6,23 +8,25 @@ import requests
 
 TOKEN = config('TOKEN')
 
-#Dados da previsão do tempo vinda da API do openweathermap.
+
+# Dados da previsão do tempo vinda da API do openweathermap.
 def get_datas():
-  data = requests.get('http://api.openweathermap.org/data/2.5/weather?id=6317872&units=metric&APPID=1d17c31f2f529e7c10426c22f5af5a6f')
-  # Parse JSON into an object with attributes corresponding to dict keys.
-  return json.loads(data.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    data = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?id=6317872&units=metric&APPID=1d17c31f2f529e7c10426c22f5af5a6f')
+    # Parse JSON into an object with attributes corresponding to dict keys.
+    return json.loads(data.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 
 
-#Updater - Dados novos enviados
-#CommandHandler - Instância de um comando.
+# Updater - Dados novos enviados
+# CommandHandler - Instância de um comando.
 
-#Função de tramento de comando
+# Função de tramento de comando
 def clima_tempo(update, context):
-    #Implemente aqui a função que vai tratar o handler/commando.
-    #Menda uma mensagem de hello;
-    #update: objeto que representa o retorno da API;
-    #message: representa a parte do retorno referente a mensagem enviada, 
-    #com dados da mensagem e metodos para manipular a mensagem;
+    # Implemente aqui a função que vai tratar o handler/commando.
+    # Menda uma mensagem de hello;
+    # update: objeto que representa o retorno da API;
+    # message: representa a parte do retorno referente a mensagem enviada,
+    # com dados da mensagem e metodos para manipular a mensagem;
     x = get_datas()
     update.message.reply_text(
         """
@@ -35,24 +39,24 @@ def clima_tempo(update, context):
         Nascer do Sol: {}
         Por do Sol: {}
         """.format(
-          x.wind.speed,
-          x.main.pressure,
-          x.main.humidity,
-          x.main.temp_min,
-          x.main.temp_max,
-          x.sys.sunrise,
-          x.sys.sunset          
-        )        
+            x.wind.speed,
+            x.main.pressure,
+            x.main.humidity,
+            x.main.temp_min,
+            x.main.temp_max,
+            datetime.fromtimestamp(x.sys.sunrise).strftime('%H:%M'),
+            datetime.fromtimestamp(x.sys.sunset).strftime('%H:%M')
         )
+    )
+
 
 updater = Updater(TOKEN, use_context=True)
 
-#Vincula função a handler/commando.
+# Vincula função a handler/commando.
 updater.dispatcher.add_handler(CommandHandler('clima_tempo', clima_tempo))
 
 updater.start_polling()
 updater.idle()
-
 
 # Referências
 # https://requests.readthedocs.io/en/master/
